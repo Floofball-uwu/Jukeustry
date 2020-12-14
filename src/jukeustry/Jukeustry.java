61b6be870f9a5b7e05023c4ff1684a567bf50d76
Farmer Thanos <wiktorlk@hotmail.com>
2020-12-14 22:18:28 +0100

    Updated Jukeustry.java

--- src/jukeustry/Jukeustry.java
+++ src/jukeustry/Jukeustry.java
@@ -1,15 +1,45 @@
 package jukeustry;
 
 import arc.*;
+import arc.scene.style.TextureRegionDrawable;
+import arc.scene.ui.Dialog;
+import arc.scene.ui.layout.Table;
 import arc.util.*;
+import jukeustry.content.JukeBlocks;
+import jukeustry.content.JukeItems;
 import mindustry.*;
 import mindustry.content.*;
 import mindustry.game.EventType.*;
 import mindustry.gen.*;
+import mindustry.graphics.Pal;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
    public class Jukeustry extends Mod {
+	   public static final String jukeustryName = "jukeustry-";
+
+	   private void links(){
+		   BaseDialog dialog = new BaseDialog("Links");
+		   addLink(dialog.cont, Icon.github, "Github", "https://github.com/FarmerThanos/Jukeustry");
+		   dialog.cont.button("Back", dialog::hide).size(120f, 60f);
+		   dialog.show();
+	   }
+
+	   private void addLink(Table table, TextureRegionDrawable icon, String buttonName, String link){
+		   table.button(buttonName, icon, () -> {
+			   Dialog dialog = new Dialog("");
+			   dialog.cont.pane(t -> {
+				   t.add("[lightgray]Are you sure jump to this link: [accent]" + link + " [lightgray]?");
+			   }).fillX().height(22f).row();
+			   dialog.cont.image().fillX().pad(8).height(4f).color(Pal.accent).row();
+			   dialog.cont.pane(t -> {
+				   t.button("Yes", Icon.link, () -> Core.app.openURI(link)).size(220f, 60f);
+				   t.button("No", Icon.cancel, dialog::hide).size(220f, 60f).pad(4);
+			   }).fillX();
+			   dialog.show();
+		   }).size(180f, 60f).left().row();
+	   }
+
 	public Jukeustry() {
 		Log.info("Loaded Jukeustry constructor.");
 	}
@@ -17,5 +47,9 @@ import mindustry.ui.dialogs.*;
 	@Override
 	public void loadContent() {
 		Log.info("Loading Jukeustry content.");
-	}
+
+		new JukeItems().load();
+		new JukeBlocks().load();
+
+	   }
 }
