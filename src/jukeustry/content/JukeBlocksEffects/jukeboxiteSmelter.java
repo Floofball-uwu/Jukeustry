e7d043817d6e11f88414cee7d740ef93d5b8ea99
Farmer Thanos <wiktorlk@hotmail.com>
2020-12-17 16:23:30 +0100

    Updated Jukeustry.java

---
+++ src/jukeustry/content/JukeBlocksEffects/jukeboxiteSmelter.java
@@ -0,0 +1,34 @@
+package jukeustry.content.JukeBlocksEffects;
+
+import arc.*;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.util.*;
+import mindustry.world.*;
+import mindustry.world.blocks.production.GenericSmelter;
+import mindustry.world.blocks.production.GenericSmelter.SmelterBuild;
+
+public class jukeboxiteSmelter extends GenericSmelter {
+    public TextureRegion colorRegion;
+
+    public jukeboxiteSmelter(String name) {
+        super(name);
+    }
+
+    @Override
+    public void load(){
+        super.load();
+        colorRegion = Core.atlas.find(name + "-color");
+    }
+
+    public class JukeboxBuild extends SmelterBuild {
+
+        @Override
+        public void draw(){
+            super.draw();
+
+            Draw.color(Color.valueOf("ff0000").shiftHue(Time.time));
+            Draw.rect(colorRegion, x, y);
+        }
+    }
+}
