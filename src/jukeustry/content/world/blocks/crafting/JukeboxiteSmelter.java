811c4c237c85a52f7a7636f00a0675f5987d228e
Farmer Thanos <wiktorlk@hotmail.com>
2020-12-19 16:24:40 +0100

    Updated code.

--- src/jukeustry/content/world/blocks/crafting/JukeboxiteSmelter.java
+++ src/jukeustry/content/world/blocks/crafting/JukeboxiteSmelter.java
@@ -27,7 +27,7 @@ public class JukeboxiteSmelter extends GenericSmelter {
         public void draw() {
 
             Draw.rect(baseSprite, x, y);
-            Draw.color(Color.valueOf("ff0000").shiftHue(Time.time()));
+            Draw.color(Color.valueOf("ff0000").shiftHue(Time.time));
             Draw.rect(colorRegion, x, y);
         }
     }
