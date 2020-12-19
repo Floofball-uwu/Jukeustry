e165941e041a71ca760ef6e2bfdc59c579e522a1
Farmer Thanos <wiktorlk@hotmail.com>
2020-12-19 23:52:36 +0100

    Updated code.

--- src/jukeustry/content/JukeBlocks.java
+++ src/jukeustry/content/JukeBlocks.java
@@ -1,6 +1,7 @@
 package jukeustry.content;
 
 import jukeustry.content.world.blocks.crafting.JukeboxiteSmelter;
+import jukeustry.content.world.blocks.logic.JukeboxBlock;
 import mindustry.content.Items;
 import mindustry.ctype.*;
 import mindustry.type.*;
@@ -15,14 +16,17 @@ import static mindustry.type.ItemStack.*;
 public class JukeBlocks implements ContentList {
     public static Block
             //production
-            jukeboxiteSmelter;
+            jukeboxiteSmelter,
+
+    //logic
+    jukeboxDoom;
 
     @Override
     public void load() {
         jukeboxiteSmelter = new JukeboxiteSmelter("jukeboxite-smelter") {{
             requirements(Category.crafting, BuildVisibility.shown, with(Items.lead, 165, Items.graphite, 65, Items.silicon, 45, Items.titanium, 110));
             size = 3;
-            health = 300;
+            health = 360;
             hasPower = true;
             hasLiquids = false;
             hasItems = true;
@@ -34,6 +38,13 @@ public class JukeBlocks implements ContentList {
 
             consumes.power(1.7f);
             consumes.items(with(Items.copper, 4, Items.titanium, 1, Items.metaglass, 2));
+        }};
+
+        jukeboxDoom = new JukeboxBlock("jukebox-doom") {{
+            requirements(Category.logic, BuildVisibility.shown, with(JukeItems.jukeboxite, 130, Items.copper, 200, Items.lead, 170, Items.silicon, 80, Items.graphite, 100));
+            size = 2;
+            health = 160;
+
         }};
     }
 }
--- src/jukeustry/content/world/blocks/logic/JukeboxBlock.java
+++ src/jukeustry/content/world/blocks/logic/JukeboxBlock.java
@@ -3,7 +3,14 @@ package jukeustry.content.world.blocks.logic;
 import mindustry.world.*;
 import mindustry.world.meta.*;
 
+import java.util.ArrayList;
+
 public class JukeboxBlock extends Block {
+
+    public int track1;
+    public int track2;
+    public int track3;
+
     public JukeboxBlock(String name) {
 
         super(name);
@@ -14,4 +21,8 @@ public class JukeboxBlock extends Block {
     }
     //Make cool jukeboxy UI here. Also finish class to allow for choosing what sound files to
     //play based on an int array from 0 (stop sound) to 16. Should allow for easy customization.
+
+    //Assign "track1", "track2", "track3", etc. to a numerical array. The tracks will be names of
+    //soundfile tracks (inputted in JukeBlocks.java), but calculated with integers for logic compatibility
+    //and flexibility.
 }
